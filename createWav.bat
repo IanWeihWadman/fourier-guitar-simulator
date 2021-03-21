b23018264638b1e1aa3d610a89612b3bb0f8ee8e
DaveWeih <dave_weih@hotmail.com>
2021-03-21 11:27:15 -0700

    Batch file

--- createWav.bat
+++ createWav.bat
@@ -2,29 +2,15 @@ SETLOCAL
 
 REM %1 is tabs file name which is also song name
 
-REM SET tabs=Data\rawSongFiles\
-REM SET strings=Data\simOut\%1\
-REM SET wavs=wavfolder\
 SET config=Release
 SET simulate=guitarSimulation\%config%\guitarSimulation.exe
 SET parse=TabParser\bin\%config%\TabParser.exe
 SET wavify=WavMaker\bin\%config%\WavMaker.exe
 
-echo %tabs%
-echo %strings%
-
-echo %parse%
-
-REM %parse% Data\rawSongFiles\lovecats.txt
-
-REM %simulate% Data\rawSongFiles\lovecats.txt
-
 if exist rawSongFiles\%1 (
-    rem file exists
-    echo exists
     %parse% %1
     %simulate% %1
-    REM %wavify% %simfolder%\output %wavs%%1
+    %wavify% %1
 ) else (
-    echo %1 not found in %tabs%
+    echo %1 not found 
 )
