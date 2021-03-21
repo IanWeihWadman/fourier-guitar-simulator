a05020cc2da1c93a39bf25fc59fcb9f2319272ed
DaveWeih <dave_weih@hotmail.com>
2021-03-21 09:44:02 -0700

    Tweaks to where files are saved & names

--- guitarSimulation/FourierGuitarSimulator.cpp
+++ guitarSimulation/FourierGuitarSimulator.cpp
@@ -23,14 +23,14 @@ int main(int argc, char** argv)
 	std::filesystem::create_directory(outputDir);
 	if (std::filesystem::exists(filePath))
 	{
-		FourierString EString(200, 0, outputDir+filePath);
+		FourierString EString(200, 0, outputDir + filePath);
 		FourierString AString(180, 1, outputDir + filePath);
 		FourierString DString(160, 2, outputDir + filePath);
 		FourierString GString(140, 3, outputDir + filePath);
 		FourierString BString(120, 4, outputDir + filePath);
 		FourierString highEString(100, 5, outputDir + filePath);
 		int totalLength = AString.totalMusicLength;
-		std::thread EThread(&FourierString::simulate, &EString, outputDir+"EFile.txt");
+		std::thread EThread(&FourierString::simulate, &EString, outputDir + "EFile.txt");
 		std::thread AThread(&FourierString::simulate, &AString, outputDir + "AFile.txt");
 		std::thread DThread(&FourierString::simulate, &DString, outputDir + "DFile.txt");
 		std::thread GThread(&FourierString::simulate, &GString, outputDir + "GFile.txt");
@@ -52,7 +52,7 @@ int main(int argc, char** argv)
 		std::ifstream GStream(outputDir + "GFile.txt");
 		std::ifstream BStream(outputDir + "BFile.txt");
 		std::ifstream eStream(outputDir + "HighEFile.txt");
-		std::ofstream output(outputDir + filePath+".out.txt");
+		std::ofstream output("simOut\\" + filePath + ".simout.txt");
 		for (int i = 0; i < totalLength + 22050; i++) {
 			value = 0;
 			EStream >> stringAddition;
