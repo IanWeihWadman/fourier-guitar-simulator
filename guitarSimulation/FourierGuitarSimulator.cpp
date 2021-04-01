3b449c4f71473f75975879587c085d730ba7002d
DaveWeih <dave_weih@hotmail.com>
2021-03-31 21:30:38 -0700

    getopt style parameters added to guitar simulator -s songname -a amp preset override -n flag to use existing guitar output and just run amp

--- guitarSimulation/FourierGuitarSimulator.cpp
+++ guitarSimulation/FourierGuitarSimulator.cpp
@@ -3,20 +3,43 @@
 #include <filesystem>
 #include "FourierString.h"
 #include "Amplifier.h"
+#include "ketopt.h"
 
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv)
 {
-	//std::string filePath;
 	std::string songName;
 	std::string fileNameString;
+	std::string ampPresetOverride;
 	std::string outputDir;
-	if (argc == 2)
-	{
-		fileNameString = argv[1];
+
+	bool no_sim = false;
+	fileNameString = "";
+
+	// options:  
+	//		s <name of tab file>			- optional, requested as console input if not available, assumed to be in ".\parseOut\"
+	//		a <amplifier preset override>	- optional, overrides amp preset from tab
+	//		n								- optional, skips simulation and only applies amplifier
+	int opt;
+	ketopt_t optarg;
+	optarg = KETOPT_INIT;
+	while ((opt = ketopt(&optarg, argc, argv, false,"a:s:n", NULL )) != -1) {
+		switch (opt) {
+		case 'n':
+			no_sim = true;
+			break;
+		case 's':
+			fileNameString = optarg.arg;
+			break;
+		case 'a':
+			ampPresetOverride = optarg.arg;
+			break;
+
+		}
 	}
-	else
+
+	if (fileNameString.length() == 0)
 	{
 		std::cin >> fileNameString;
 	}
@@ -29,61 +52,37 @@ int main(int argc, char** argv)
 	fs::create_directory(outputDir);
 	if (fs::exists(inputPathString))
 	{
-		FourierString AString(180, 1, inputPathString);
+		FourierString AString(180, 1, inputPathString); // need this one to read song info, if that's removed or accessed differently this could be skipped as well
 		int totalLength = AString.totalMusicLength;
-		FourierString BString(120, 4, inputPathString);
-		FourierString DString(160, 2, inputPathString);
-		FourierString highEString(100, 5, inputPathString);
-		FourierString EString(200, 0, inputPathString);
-		FourierString GString(140, 3, inputPathString);
-		std::thread EThread(&FourierString::simulate, &EString, outputDir + "EFile.txt");
-		std::thread AThread(&FourierString::simulate, &AString, outputDir + "AFile.txt");
-		std::thread DThread(&FourierString::simulate, &DString, outputDir + "DFile.txt");
-		std::thread GThread(&FourierString::simulate, &GString, outputDir + "GFile.txt");
-		std::thread BThread(&FourierString::simulate, &BString, outputDir + "BFile.txt");
-		std::thread highEThread(&FourierString::simulate, &highEString, outputDir + "HighEFile.txt");
-		EThread.join();
-		AThread.join();
-		DThread.join();
-		GThread.join();
-		BThread.join();
-		highEThread.join();
-		double value = 0;
-		double stringAddition = 0;
-		double processedValue = 0;
-		std::string comma;
-		/*for (int i = 0; i < totalLength + 22050; i++) {
-			value = 0;
-			EStream >> stringAddition;
-			value += stringAddition;
-			EStream >> comma;
-			AStream >> stringAddition;
-			value += stringAddition;
-			AStream >> comma;
-			DStream >> stringAddition;
-			value += stringAddition;
-			DStream >> comma;
-			GStream >> stringAddition;
-			value += stringAddition;
-			GStream >> comma;
-			BStream >> stringAddition;
-			value += stringAddition;
-			BStream >> comma;
-			eStream >> stringAddition;
-			value += stringAddition;
-			eStream >> comma;
-			processedValue = 0.6 * value;
-			if (processedValue > 1.8) {
-				processedValue = 1.8;
-			}
-			if (processedValue < -1.8) {
-				processedValue = -1.8;
-			}
-			processedValue = processedValue / (1 + 0.3 * processedValue * processedValue);
-			output << processedValue << ", ";
+		if (!no_sim)
+		{
+			FourierString AString(180, 1, inputPathString); // put here for hacking 
+
+			FourierString BString(120, 4, inputPathString);
+			FourierString DString(160, 2, inputPathString);
+			FourierString highEString(100, 5, inputPathString);
+			FourierString EString(200, 0, inputPathString);
+			FourierString GString(140, 3, inputPathString);
+			std::thread EThread(&FourierString::simulate, &EString, outputDir + "EFile.txt");
+			std::thread AThread(&FourierString::simulate, &AString, outputDir + "AFile.txt");
+			std::thread DThread(&FourierString::simulate, &DString, outputDir + "DFile.txt");
+			std::thread GThread(&FourierString::simulate, &GString, outputDir + "GFile.txt");
+			std::thread BThread(&FourierString::simulate, &BString, outputDir + "BFile.txt");
+			std::thread highEThread(&FourierString::simulate, &highEString, outputDir + "HighEFile.txt");
+			EThread.join();
+			AThread.join();
+			DThread.join();
+			GThread.join();
+			BThread.join();
+			highEThread.join();
+			double value = 0;
+			double stringAddition = 0;
+			double processedValue = 0;
 		}
-		output.close();*/
-		Amplifier amp(outputDir + "EFile.txt", outputDir + "AFile.txt", outputDir + "DFile.txt", outputDir + "GFile.txt", outputDir + "BFile.txt", outputDir + "HighEFile.txt", outputDir + "output.txt", AString.getAmpPreset(), totalLength);
+
+		std::string ampPreset = (ampPresetOverride.length() > 0) ? ampPresetOverride : AString.getAmpPreset();
+
+		Amplifier amp(outputDir + "EFile.txt", outputDir + "AFile.txt", outputDir + "DFile.txt", outputDir + "GFile.txt", outputDir + "BFile.txt", outputDir + "HighEFile.txt", outputDir + "output.txt", ampPreset, totalLength);
 	}
 	else
 	{
